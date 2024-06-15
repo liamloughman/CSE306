315a295711912a97f880f074f0cbac6b41b8f94b
liamloughman <liam.loughman@polytechnique.edu>
2024-06-16 00:27:26 +0200

    Implemented Lab-9, does not work

---
+++ Lab-9/main.cpp
@@ -0,0 +1,340 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+
+class Vector {
+public:
+	explicit Vector(double x = 0, double y = 0, double z = 0) {
+		data[0] = x;
+		data[1] = y;
+        data[2] = z;
+	}
+	double norm2() const {
+		return data[0] * data[0] + data[1] * data[1] + data[2] * data[2];
+	}
+	double norm() const {
+        return sqrt(norm2());
+    }
+	Vector normalize() const {
+		double n = norm();
+		return Vector(data[0] / n, data[1] / n, data[2] / n);
+	}
+	double operator[](int i) const { return data[i]; };
+	double& operator[](int i) { return data[i]; };
+	double data[2];
+};
+
+Vector operator+(const Vector& a, const Vector& b) {
+	return Vector(a[0] + b[0], a[1] + b[1], a[2] + b[2]);
+}
+Vector operator-(const Vector& a, const Vector& b) {
+	return Vector(a[0] - b[0], a[1] - b[1], a[2] - b[2]);
+}
+Vector operator*(const double a, const Vector& b) {
+	return Vector(a*b[0], a*b[1], a*b[2]);
+}
+Vector operator*(const Vector& a, const double b) {
+	return Vector(a[0]*b, a[1]*b, a[2]*b);
+}
+Vector operator*(const Vector& a, const Vector& b) {
+    return Vector(a[0]*b[0], a[1]*b[1], a[2]*b[2]);
+}
+Vector operator/(const Vector& a, const double b) {
+	return Vector(a[0] / b, a[1] / b, a[2] / b);
+}
+double dot(const Vector& a, const Vector& b) {
+	return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
+
+class Polygon {  
+public:
+
+	std::vector<Vector> vertices;
+
+	Polygon() {};
+
+	explicit Polygon(const std::vector<Vector> &vertices) {
+        this->vertices = vertices;
+    };
+
+    void add(const Vector& a) {
+        this->vertices.push_back(a);
+    };
+};
+
+class TriangleIndices {
+public:
+	TriangleIndices(int vtxi = -1, int vtxj = -1, int vtxk = -1, int ni = -1, int nj = -1, int nk = -1, int uvi = -1, int uvj = -1, int uvk = -1, int group = -1, bool added = false) : vtxi(vtxi), vtxj(vtxj), vtxk(vtxk), uvi(uvi), uvj(uvj), uvk(uvk), ni(ni), nj(nj), nk(nk), group(group) {
+	};
+	int vtxi, vtxj, vtxk; // indices within the vertex coordinates array
+	int uvi, uvj, uvk;  // indices within the uv coordinates array
+	int ni, nj, nk;  // indices within the normals array
+	int group;       // face group
+};
+
+
+class TriangleMesh {
+public:
+  ~TriangleMesh() {}
+	TriangleMesh() {};
+	
+	void readOBJ(const char* obj) {
+
+		char matfile[255];
+		char grp[255];
+
+		FILE* f;
+		f = fopen(obj, "r");
+		int curGroup = -1;
+		while (!feof(f)) {
+			char line[255];
+			if (!fgets(line, 255, f)) break;
+
+			std::string linetrim(line);
+			linetrim.erase(linetrim.find_last_not_of(" \r\t") + 1);
+			strcpy(line, linetrim.c_str());
+
+			if (line[0] == 'u' && line[1] == 's') {
+				sscanf(line, "usemtl %[^\n]\n", grp);
+				curGroup++;
+			}
+
+			if (line[0] == 'v' && line[1] == ' ') {
+				Vector vec;
+
+				Vector col;
+				if (sscanf(line, "v %lf %lf %lf %lf %lf %lf\n", &vec[0], &vec[1], &vec[2], &col[0], &col[1], &col[2]) == 6) {
+					col[0] = std::min(1., std::max(0., col[0]));
+					col[1] = std::min(1., std::max(0., col[1]));
+					col[2] = std::min(1., std::max(0., col[2]));
+
+					vertices.push_back(vec);
+					vertexcolors.push_back(col);
+
+				} else {
+					sscanf(line, "v %lf %lf %lf\n", &vec[0], &vec[1], &vec[2]);
+					vertices.push_back(vec);
+				}
+			}
+			if (line[0] == 'v' && line[1] == 'n') {
+				Vector vec;
+				sscanf(line, "vn %lf %lf %lf\n", &vec[0], &vec[1], &vec[2]);
+				normals.push_back(vec);
+			}
+			if (line[0] == 'v' && line[1] == 't') {
+				Vector vec;
+				sscanf(line, "vt %lf %lf\n", &vec[0], &vec[1]);
+				uvs.push_back(vec);
+			}
+			if (line[0] == 'f') {
+				TriangleIndices t;
+				int i0, i1, i2, i3;
+				int j0, j1, j2, j3;
+				int k0, k1, k2, k3;
+				int nn;
+				t.group = curGroup;
+
+				char* consumedline = line + 1;
+				int offset;
+
+				nn = sscanf(consumedline, "%u/%u/%u %u/%u/%u %u/%u/%u%n", &i0, &j0, &k0, &i1, &j1, &k1, &i2, &j2, &k2, &offset);
+				if (nn == 9) {
+					if (i0 < 0) t.vtxi = vertices.size() + i0; else	t.vtxi = i0 - 1;
+					if (i1 < 0) t.vtxj = vertices.size() + i1; else	t.vtxj = i1 - 1;
+					if (i2 < 0) t.vtxk = vertices.size() + i2; else	t.vtxk = i2 - 1;
+					if (j0 < 0) t.uvi = uvs.size() + j0; else	t.uvi = j0 - 1;
+					if (j1 < 0) t.uvj = uvs.size() + j1; else	t.uvj = j1 - 1;
+					if (j2 < 0) t.uvk = uvs.size() + j2; else	t.uvk = j2 - 1;
+					if (k0 < 0) t.ni = normals.size() + k0; else	t.ni = k0 - 1;
+					if (k1 < 0) t.nj = normals.size() + k1; else	t.nj = k1 - 1;
+					if (k2 < 0) t.nk = normals.size() + k2; else	t.nk = k2 - 1;
+					indices.push_back(t);
+				} else {
+					nn = sscanf(consumedline, "%u/%u %u/%u %u/%u%n", &i0, &j0, &i1, &j1, &i2, &j2, &offset);
+					if (nn == 6) {
+						if (i0 < 0) t.vtxi = vertices.size() + i0; else	t.vtxi = i0 - 1;
+						if (i1 < 0) t.vtxj = vertices.size() + i1; else	t.vtxj = i1 - 1;
+						if (i2 < 0) t.vtxk = vertices.size() + i2; else	t.vtxk = i2 - 1;
+						if (j0 < 0) t.uvi = uvs.size() + j0; else	t.uvi = j0 - 1;
+						if (j1 < 0) t.uvj = uvs.size() + j1; else	t.uvj = j1 - 1;
+						if (j2 < 0) t.uvk = uvs.size() + j2; else	t.uvk = j2 - 1;
+						indices.push_back(t);
+					} else {
+						nn = sscanf(consumedline, "%u %u %u%n", &i0, &i1, &i2, &offset);
+						if (nn == 3) {
+							if (i0 < 0) t.vtxi = vertices.size() + i0; else	t.vtxi = i0 - 1;
+							if (i1 < 0) t.vtxj = vertices.size() + i1; else	t.vtxj = i1 - 1;
+							if (i2 < 0) t.vtxk = vertices.size() + i2; else	t.vtxk = i2 - 1;
+							indices.push_back(t);
+						} else {
+							nn = sscanf(consumedline, "%u//%u %u//%u %u//%u%n", &i0, &k0, &i1, &k1, &i2, &k2, &offset);
+							if (i0 < 0) t.vtxi = vertices.size() + i0; else	t.vtxi = i0 - 1;
+							if (i1 < 0) t.vtxj = vertices.size() + i1; else	t.vtxj = i1 - 1;
+							if (i2 < 0) t.vtxk = vertices.size() + i2; else	t.vtxk = i2 - 1;
+							if (k0 < 0) t.ni = normals.size() + k0; else	t.ni = k0 - 1;
+							if (k1 < 0) t.nj = normals.size() + k1; else	t.nj = k1 - 1;
+							if (k2 < 0) t.nk = normals.size() + k2; else	t.nk = k2 - 1;
+							indices.push_back(t);
+						}
+					}
+				}
+
+				consumedline = consumedline + offset;
+
+				while (true) {
+					if (consumedline[0] == '\n') break;
+					if (consumedline[0] == '\0') break;
+					nn = sscanf(consumedline, "%u/%u/%u%n", &i3, &j3, &k3, &offset);
+					TriangleIndices t2;
+					t2.group = curGroup;
+					if (nn == 3) {
+						if (i0 < 0) t2.vtxi = vertices.size() + i0; else	t2.vtxi = i0 - 1;
+						if (i2 < 0) t2.vtxj = vertices.size() + i2; else	t2.vtxj = i2 - 1;
+						if (i3 < 0) t2.vtxk = vertices.size() + i3; else	t2.vtxk = i3 - 1;
+						if (j0 < 0) t2.uvi = uvs.size() + j0; else	t2.uvi = j0 - 1;
+						if (j2 < 0) t2.uvj = uvs.size() + j2; else	t2.uvj = j2 - 1;
+						if (j3 < 0) t2.uvk = uvs.size() + j3; else	t2.uvk = j3 - 1;
+						if (k0 < 0) t2.ni = normals.size() + k0; else	t2.ni = k0 - 1;
+						if (k2 < 0) t2.nj = normals.size() + k2; else	t2.nj = k2 - 1;
+						if (k3 < 0) t2.nk = normals.size() + k3; else	t2.nk = k3 - 1;
+						indices.push_back(t2);
+						consumedline = consumedline + offset;
+						i2 = i3;
+						j2 = j3;
+						k2 = k3;
+					} else {
+						nn = sscanf(consumedline, "%u/%u%n", &i3, &j3, &offset);
+						if (nn == 2) {
+							if (i0 < 0) t2.vtxi = vertices.size() + i0; else	t2.vtxi = i0 - 1;
+							if (i2 < 0) t2.vtxj = vertices.size() + i2; else	t2.vtxj = i2 - 1;
+							if (i3 < 0) t2.vtxk = vertices.size() + i3; else	t2.vtxk = i3 - 1;
+							if (j0 < 0) t2.uvi = uvs.size() + j0; else	t2.uvi = j0 - 1;
+							if (j2 < 0) t2.uvj = uvs.size() + j2; else	t2.uvj = j2 - 1;
+							if (j3 < 0) t2.uvk = uvs.size() + j3; else	t2.uvk = j3 - 1;
+							consumedline = consumedline + offset;
+							i2 = i3;
+							j2 = j3;
+							indices.push_back(t2);
+						} else {
+							nn = sscanf(consumedline, "%u//%u%n", &i3, &k3, &offset);
+							if (nn == 2) {
+								if (i0 < 0) t2.vtxi = vertices.size() + i0; else	t2.vtxi = i0 - 1;
+								if (i2 < 0) t2.vtxj = vertices.size() + i2; else	t2.vtxj = i2 - 1;
+								if (i3 < 0) t2.vtxk = vertices.size() + i3; else	t2.vtxk = i3 - 1;
+								if (k0 < 0) t2.ni = normals.size() + k0; else	t2.ni = k0 - 1;
+								if (k2 < 0) t2.nj = normals.size() + k2; else	t2.nj = k2 - 1;
+								if (k3 < 0) t2.nk = normals.size() + k3; else	t2.nk = k3 - 1;								
+								consumedline = consumedline + offset;
+								i2 = i3;
+								k2 = k3;
+								indices.push_back(t2);
+							} else {
+								nn = sscanf(consumedline, "%u%n", &i3, &offset);
+								if (nn == 1) {
+									if (i0 < 0) t2.vtxi = vertices.size() + i0; else	t2.vtxi = i0 - 1;
+									if (i2 < 0) t2.vtxj = vertices.size() + i2; else	t2.vtxj = i2 - 1;
+									if (i3 < 0) t2.vtxk = vertices.size() + i3; else	t2.vtxk = i3 - 1;
+									consumedline = consumedline + offset;
+									i2 = i3;
+									indices.push_back(t2);
+								} else {
+									consumedline = consumedline + 1;
+								}
+							}
+						}
+					}
+				}
+
+			}
+
+		}
+		fclose(f);
+
+	}
+
+	std::vector<TriangleIndices> indices;
+	std::vector<Vector> vertices;
+	std::vector<Vector> normals;
+	std::vector<Vector> uvs;
+	std::vector<Vector> vertexcolors;
+	
+};
+
+std::vector<int> find_boundary_vertices(const std::vector<std::vector<int>>& adjacency_list) {
+    std::vector<int> boundary_vertices;
+
+    for (int i = 0; i < adjacency_list.size(); ++i) {
+        if (adjacency_list[i].size() <= 3) {
+            boundary_vertices.push_back(i);
+        }
+    }
+
+    return boundary_vertices;
+}
+
+void build_adjacency_list(TriangleMesh &mesh, std::vector<std::vector<int>> &adjacency_list) {
+    for (const auto& tri : mesh.indices) {
+        int vertices[3] = {tri.vtxi, tri.vtxj, tri.vtxk};
+        for (int i = 0; i < 3; ++i) {
+            int v1 = vertices[i];
+            int v2 = vertices[(i + 1) % 3];
+            adjacency_list[v1].push_back(v2);
+            adjacency_list[v2].push_back(v1);
+        }
+    }
+    for (auto& neighbors : adjacency_list) {
+        std::sort(neighbors.begin(), neighbors.end());
+        neighbors.erase(std::unique(neighbors.begin(), neighbors.end()), neighbors.end());
+    }
+}
+
+std::vector<Vector> tutte_embedding(TriangleMesh &mesh, const std::vector<int> &boundary_vertices, std::vector<std::vector<int>> &adjacency_list, int nbr_iterations) {
+    int n = boundary_vertices.size();
+    std::vector<Vector> v(mesh.vertices.size());
+    double s = 0.0;
+    for (int i = 0; i < n; i++) {
+        s += (mesh.vertices[boundary_vertices[(i + 1)%n]] - mesh.vertices[boundary_vertices[i]]).norm();
+    }
+    double cs = 0.0;
+    for (int i = 0; i < n; i++) {
+        double theta = 2*M_PI*cs/s;
+        v[boundary_vertices[i]] = Vector(cos(theta), sin(theta), 0);
+        cs += (mesh.vertices[boundary_vertices[(i + 1)%n]] - mesh.vertices[boundary_vertices[i]]).norm();
+    }
+    for (int iter = 0; iter < nbr_iterations; ++iter) {
+        std::vector<Vector> new_positions = v;
+        for (int i = 0; i < mesh.vertices.size(); i++) {
+            if (std::find(boundary_vertices.begin(), boundary_vertices.end(), i) == boundary_vertices.end()) {
+                Vector sum(0, 0, 0);
+                int K = 0;
+                for (int neighbor : adjacency_list[i]) {
+                    sum = sum + v[neighbor];
+                    K++;
+                }
+                if (K > 0) {
+                    new_positions[i] = sum/K;
+                }
+            }
+        }
+        v = new_positions;
+    }
+    return v;
+}
+
+int main() {
+    TriangleMesh* mesh;
+    mesh->readOBJ("mask.obj");
+    int nbr_iterations = 100;
+    std::vector<std::vector<int>> adjacency_list;
+    build_adjacency_list(*mesh, adjacency_list);
+    // mesh.adjacency_list = adjacency_list;
+    std::vector<int> boundary_vertices = find_boundary_vertices(adjacency_list);
+    std::vector<Vector> embedded_positions = tutte_embedding(*mesh, boundary_vertices, adjacency_list, nbr_iterations);
+    for (const Vector &v : embedded_positions) {
+        std::cout << "Vertex: (" << v[0] << ", " << v[1] << ", " << v[2] << ")" << std::endl;
+    }
+}
